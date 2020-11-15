a9b9c4cb43f3b2a8e8ba3e2a9013e23764f7ae73
Mateus Goulart <mat_goulart3003@yahoo.com.br>
2020-11-14 21:32:03 -0300

    opção 2 feita

--- Campeonato de Futebol/src/br/edu/univas/main/StartApp.java
+++ Campeonato de Futebol/src/br/edu/univas/main/StartApp.java
@@ -18,15 +18,23 @@ public class StartApp {
 		
 		do {
 			
-			System.out.println("Bem vindo ao sistema de CAMPEONATO DE FUTEBOL: ");
+			System.out.println("\n\nBem vindo ao sistema de CAMPEONATO DE FUTEBOL: ");
 			screenOptions();
 			
 			int option = scan.nextInt();
 			scan.nextLine();
-			selectOption(option);
 			
+			selectOption(option, equipe, partida);
 			
-			break;
+			if (option == 9) {
+				break;
+			
+			} else if (option != 1 && option != 2 && option != 3 && option != 4 && option != 5 && option != 6 && option != 7) {
+				
+				System.out.println("Opção inválida!!");
+				
+			}
+			
 			
 		}while (true);
 		
@@ -34,16 +42,61 @@ public class StartApp {
 		
 	}
 	
-	public static void selectOption(int option) {
+	public static void selectOption(int option, Equipe equipe [], Partida partida []) {
 		
+		Equipe time = new Equipe();		
 		
+		// opção de criar equipe
 		
 		if (option == 1) {
 			
-			Equipe equipe = new Equipe();
-			preencheTime(equipe);
+			preencheTime(time);
+			
+			for (int i = 0; i < tamanhoArray; i++) {
+				
+				if (equipe[i] == null) {
+					
+					equipe[i] = time;
+					break;
+				}
+				
+			}
 			
 		}
+		
+		// edita time
+		else if (option == 2) {
+			
+			System.out.println("Qual equipe você quer editar?");
+			
+			for (int i = 0; i < tamanhoArray; i++) {
+				
+				if (equipe[i] != null) {
+					
+					System.out.println( i + " " + equipe[i].nome);
+					
+				}
+				
+			}
+			
+			System.out.println("Digite o indice do time desejado: ");
+			int indice = scan.nextInt();
+			scan.nextLine();
+			
+			for (int i = 0; i < tamanhoArray; i++) {
+				
+				if (i == indice) {
+					
+					preencheTime (time);
+					equipe[i] = time;
+					break;
+				}
+				
+			}
+			
+		}
+		
+		//
 		
 	}
 	
@@ -60,7 +113,7 @@ public class StartApp {
 
 	public static void screenOptions () {
 		
-		System.out.println("==========================================");
+		System.out.println("\n==========================================");
 		System.out.println("Selecione uma ação à seguir!");
 		System.out.println("1 - Cadastrar time");
 		System.out.println("2 - Editar time");
