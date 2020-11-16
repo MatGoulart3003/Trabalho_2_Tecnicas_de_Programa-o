4771d7c0726a5523fdbfb6beb8f06de6c8787055
Mateus Goulart <mat_goulart3003@yahoo.com.br>
2020-11-15 21:31:52 -0300

    opção 5 feita

--- Campeonato de Futebol/src/br/edu/univas/main/StartApp.java
+++ Campeonato de Futebol/src/br/edu/univas/main/StartApp.java
@@ -45,7 +45,7 @@ public class StartApp {
 	public static void selectOption(int option, Equipe equipe [], Partida partida []) {
 		
 		Equipe time = new Equipe();		
-		
+		Partida jogo = new Partida();
 		// opção de criar equipe
 		
 		if (option == 1) {
@@ -59,23 +59,46 @@ public class StartApp {
 		
 		else if (option == 2) {
 			
-			System.out.println("Qual equipe você quer editar?");
+			System.out.println("Qual equipe você quer editar?\n");
 			searchTeam(equipe);			
 			int indice = scanIdx();			
-			editArray(indice, equipe, time);
+			editArrayTeam(indice, equipe, time);
 			
 		}
 		
-		// opção 3
+		// exclui time
 		
 		else if (option == 3) {
 			
-			System.out.println("Qual equipe você quer excluir?");
+			System.out.println("Qual equipe você quer excluir?\n");
 			searchTeam(equipe);	
 			int indice = scanIdx();				
 			deleteTeam(equipe, indice);
 				
 		}
+		
+		// cria jogo
+		
+		else if (option == 4) {
+			
+			int indice = cadastraMandante(equipe,jogo);
+			int indice2 = cadastraVisitante(equipe,jogo);
+			logicScore(equipe,jogo,indice,indice2);
+			addGameArray(partida,jogo);
+			
+		}
+		
+		// edita jogo
+		
+		else if (option == 5) {
+			
+			System.out.println("Qual jogo você quer editar?\n");
+			searchGame(partida);
+			int indice = scanIdx();
+			editArrayGame(indice, partida, jogo, equipe);
+			
+		}
+		
 		
 	}
 	
@@ -119,7 +142,7 @@ public class StartApp {
 		
 	}
 
-	public static void editArray (int indice, Equipe equipe [], Equipe time) {
+	public static void editArrayTeam (int indice, Equipe equipe [], Equipe time) {
 		
 		for (int i = 0; i < rangeArray; i++) {
 			
@@ -171,5 +194,101 @@ public class StartApp {
 		
 	}
 	
+	public static int cadastraMandante (Equipe equipe [], Partida jogo) {
+		
+		System.out.println("Selecione as equipes que voce quer cadastrar a partida:\n ");
+		searchTeam(equipe);	
+		System.out.println("\nTIME MANDANTE\n ");
+		int indice = scanIdx();
+		jogo.mandante = equipe[indice];
+		
+		System.out.println("Digite agora os gols do time mandante: ");
+		jogo.golMand = scan.nextInt();
+		scan.nextLine();
+		
+		return indice;
+		
+	}
+	
+	public static int cadastraVisitante (Equipe equipe[], Partida jogo) {
+		
+		searchTeam(equipe);
+		System.out.println("\nTIME VISITANTE\n ");
+		int indice = scanIdx();
+		jogo.visitante = equipe[indice];
+		
+		System.out.println("Digite agora os gols do time visitante: ");
+		jogo.golVisit = scan.nextInt();
+		scan.nextLine();
+		
+		return indice;
+	}
+
+	public static void logicScore(Equipe equipe[],Partida jogo, int indice, int indice2) {
+		
+		if(jogo.golMand > jogo.golVisit) {
+			
+			equipe[indice].pontos = equipe[indice].pontos + 3;
+			equipe[indice].saldoGols = jogo.golMand - jogo.golVisit;
+			equipe[indice2].saldoGols = jogo.golVisit - jogo.golMand;
+			
+		
+		}else if (jogo.golVisit > jogo.golMand) {
+			
+			equipe[indice2].pontos = equipe[indice2].pontos + 3;
+			equipe[indice2].saldoGols = jogo.golVisit - jogo.golVisit;
+			equipe[indice].saldoGols = jogo.golMand - jogo.golVisit;
+			
+		}
+		
+	}
+	
+	public static void addGameArray (Partida partida [], Partida jogo) {
+		
+		for (int i = 0; i < rangeArray; i++) {
+			
+			if (partida[i] == null) {
+				
+				partida[i] = jogo;
+				break;
+			}
+			
+		}
+		
+	}
+	
+	public static void searchGame (Partida partida []) {
+		
+		for (int i = 0; i < rangeArray; i++) {
+			
+			if (partida[i] != null) {
+				
+				System.out.println( i + " - " + partida[i].mandante.nome + " X " + partida[i].visitante.nome);
+				
+			}
+			
+		}
+		
+	}
+	
+	public static void editArrayGame (int idx, Partida partida[], Partida jogo, Equipe equipe []) {
+		
+		for (int i = 0; i < rangeArray; i++) {
+			
+			if (i == idx) {
+				
+				int indice = cadastraMandante(equipe,jogo);
+				int indice2 = cadastraVisitante(equipe,jogo);
+				logicScore(equipe,jogo,indice,indice2);
+				addGameArray(partida,jogo);
+				partida[i] = jogo;
+				break;
+			
+			}
+			
+		}
+
+		
+	}
 	
 }
--- Campeonato de Futebol/src/br/edu/univas/vo/Equipe.java
+++ Campeonato de Futebol/src/br/edu/univas/vo/Equipe.java
@@ -4,5 +4,6 @@ public class Equipe {
 	
 	public String nome;
 	public String estado;
-	
+	public int pontos = 0;
+	public int saldoGols = 0;
 }
